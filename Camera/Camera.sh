d3f30c39242f1327d75b149c6f4eb1847d37e169
rasp2-cho <rumidier@gmail.com>
2015-09-24 18:01:17 +0900

    Change camera path

--- Camera/Camera.sh
+++ Camera/Camera.sh
@@ -1,3 +1,3 @@
-export STREAMER_PATH=$HOME/workspace/Project/Camera/mjpg-streamer-experimental
+export STREAMER_PATH=$HOME/workspace/Project/Camera/mjpg-streamer
 export LD_LIBRARY_PATH=$STREAMER_PATH
 $STREAMER_PATH/mjpg_streamer -i "input_uvc.so -f 20 -r 200x120 -d /dev/video0 " -o "output_http.so -w $STREAMER_PATH/www"
